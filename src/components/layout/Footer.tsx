4855f51dffaf32bdd058852057daf49208a7cb90
Aaron Ma <stu1030113@gmail.com>
2026-01-17 17:37:59 +0800

    Add footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,7 @@
+import React from "react";
 const Footer: React.FC = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-red-50 pt-10 pb-14 w-11/12 md:w-4/5 m-auto">Footer</div>
+  );
 };
 export default Footer;
